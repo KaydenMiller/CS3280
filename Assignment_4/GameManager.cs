b55fe783c25de1ae2961d422608fb08d7f2d33ff
Kayden Miller <kaydenmiller1@gmail.com>
2018-10-02 09:44:29 -0600

    Started work on Assignment_4. Added RoundManager and GameManager classes. Added GameEvent delegate to controll game events. Finished most of the UI items.

---
+++ Assignment_4/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class GameManager
+    {
+        private uint player1Score = 0;
+        private uint player2Score = 0;
+        private uint ties = 0;
+
+        private RoundManager currentRoundManager;
+
+        public delegate void GameEvent();
+        public event GameEvent OnGameReset;
+
+        public GameManager()
+        {
+            // Set up events
+            OnGameReset += ResetGame;
+        }
+
+        public void StartNewGame()
+        {
+            currentRoundManager = new RoundManager();
+        }
+
+        private void ResetGame()
+        {
+            player1Score = 0;
+            player2Score = 0;
+            ties = 0;
+        }
+    }
+}
---
+++ Assignment_4/RoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class RoundManager
+    {
+        private int[,] gameBoard = new int[3, 3];
+        private int currentRound = 0;
+        
+        private enum Player { Player1, Player2 }
+        private Player currentPlayer = Player.Player1;
+
+        private void NextRound()
+        {
+            ++currentRound;
+
+            switch(currentPlayer)
+            {
+                case Player.Player1:
+                    currentPlayer = Player.Player2;
+                    break;
+                case Player.Player2:
+                    currentPlayer = Player.Player1;
+                    break;
+            }
+        }
+    }
+}
