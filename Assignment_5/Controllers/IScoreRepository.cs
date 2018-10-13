1d3f7847d1ceed07b49d16ab524fd18131167f49
Kayden Miller <kaydenmiller1@gmail.com>
2018-10-12 18:22:15 -0600

    Add the score repository interface Add a class to keep track of score information

---
+++ Assignment_5/Controllers/IScoreRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5.Controllers
+{
+    public interface IScoreRepository
+    {
+        // Add CRUD operations methods
+        // Create
+        void Add(Score score);
+        void AddScores(IEnumerable<Score> scores);
+        // Read
+        Score GetScore(int index);
+        IEnumerable<Score> GetScores();
+        // Update
+        void UpdateScores(IEnumerable<Score> scores);
+        void UpdateScore(int index, Score score);
+        // Delete
+        void Delete(int index);
+        void DeleteScores();
+    }
+}
